02fe723a5482c0b5d4862e76e070ba7525b000d8
Yoni Lerner <yoni.lerner@discordapp.com>
2022-02-09 21:06:54 -0800

    sticker audit log

--- src/commands/chat/sticker.ts
+++ src/commands/chat/sticker.ts
@@ -1,32 +1,55 @@
 import {SlashCommand} from 'slashasaurus'
+import {auditLogReport} from '../../utils'
+import {MessageEmbed} from 'discord.js'
 
-export default new SlashCommand({
-    name: 'sticker',
-    description: 'Send a random yone sticker',
-    options: []
-}, {
-    async run(interaction, client) {
-        const guild = await interaction.guild?.fetch()
-        if (!guild) {
-            return interaction.reply({content: 'Watcha doin', ephemeral: true})
-        }
-        const stickers = await guild.stickers.fetch()
-        const randomSticker = stickers.at(Math.floor((Math.random() * stickers.size)))
-        if (!randomSticker) {
-            return interaction.reply({content: 'Aint no stickers', ephemeral: true})
-        }
-        const channel = await interaction.channel?.fetch()
-        if (!channel) {
-            return interaction.reply({content: 'WAT DOING', ephemeral: true})
-        }
+export default new SlashCommand(
+    {
+        name: 'sticker',
+        description: 'Send a random yone sticker',
+        options: [],
+    },
+    {
+        async run(interaction, client) {
+            const guild = await interaction.guild?.fetch()
+            if (!guild) {
+                return interaction.reply({content: 'Watcha doin', ephemeral: true})
+            }
+            const stickers = await guild.stickers.fetch()
+            const randomSticker = stickers.at(Math.floor(Math.random() * stickers.size))
+            if (!randomSticker) {
+                return interaction.reply({content: 'Aint no stickers', ephemeral: true})
+            }
+            const channel = await interaction.channel?.fetch()
+            if (!channel) {
+                return interaction.reply({content: 'WAT DOING', ephemeral: true})
+            }
 
-        await channel.send({
-            stickers: [randomSticker]
-        })
+            const message = await channel.send({
+                stickers: [randomSticker],
+            })
 
-        return interaction.reply({
-            ephemeral: true,
-            content: `I sent ${randomSticker.name}`
-        })
-    }
-})
+            Promise.resolve().then(async () => {
+                const member = await guild.members.fetch(interaction.user.id)
+                await auditLogReport(
+                    {
+                        embeds: [
+                            new MessageEmbed()
+                                .setAuthor({
+                                    name: member.displayName,
+                                    iconURL: member.displayAvatarURL(),
+                                })
+                                .setDescription(`${member} sent ${randomSticker.name}`)
+                                .setTitle(message.url),
+                        ],
+                    },
+                    guild,
+                )
+            })
+
+            return interaction.reply({
+                ephemeral: true,
+                content: `I sent ${randomSticker.name}`,
+            })
+        },
+    },
+)
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,29 @@
+import {Guild, MessageOptions, MessagePayload, TextChannel} from 'discord.js'
+
 export const EMOJIS = {
     star: '⭐',
+}
+
+export async function getAuditLogChannel(guild: Guild): Promise<TextChannel | undefined> {
+    let channel = guild.channels.cache.find((channel) => channel.name === 'audit-log')
+    if (channel instanceof TextChannel) {
+        return channel
+    }
+    const channels = await guild.channels.fetch()
+    channel = channels.find((channel) => channel.name === 'audit-log')
+    if (channel instanceof TextChannel) {
+        return channel
+    }
+    return undefined
+}
+
+export async function auditLogReport(
+    message: string | MessagePayload | MessageOptions,
+    guild: Guild,
+) {
+    const auditLogChannel = await getAuditLogChannel(guild)
+    if (!auditLogChannel) {
+        return
+    }
+    await auditLogChannel.send(message)
 }
